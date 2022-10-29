a7c3dc1b332da14f1e3a3abc12843f2aec3abb4b
Miroslav Hájek <xhajekm@stuba.sk>
2022-10-29 23:35:39 +0200

    NAI-50 Autogenerate migration of users table from model

---
+++ migrations/versions/0afdb03f9fea_create_users_table.py
@@ -0,0 +1,33 @@
+"""Create users table
+
+Revision ID: 0afdb03f9fea
+Revises:
+Create Date: 2022-10-29 23:01:58.221745
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0afdb03f9fea'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table('users',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('username', sa.String(), nullable=False),
+        sa.Column('password', sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint('id'),
+        sa.UniqueConstraint('username')
+    )
+    op.create_index(op.f('ix_users_id'), 'users', ['id'], unique=False)
+
+
+def downgrade() -> None:
+    op.drop_index(op.f('ix_users_id'), table_name='users')
+    op.drop_table('users')
+
