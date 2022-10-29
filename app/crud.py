7f133690bdc135920d8fd86a4372107ca74cc82f
Miroslav Hájek <xhajekm@stuba.sk>
2022-10-29 23:32:18 +0200

    NAI-51 Add user registration endpoint

--- app/crud.py
+++ app/crud.py
@@ -1,9 +1,27 @@
 from sqlalchemy import select, update, delete
 from sqlalchemy.sql import func
 from sqlalchemy.sql.expression import cast
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession
+from passlib.hash import argon2
+
 import app.model as m
+import app.schema as s
 
 
-def sample_text():
-    return select(m.Example.name)
- 
+async def create_user(user: s.UserCreate):
+    hashed_password = argon2.hash(user.password)
+    user_model = m.User(
+        username=user.username,
+        password=hashed_password
+    )
+
+    async_session = sessionmaker(
+        m.engine, expire_on_commit=False, class_=AsyncSession
+    )
+
+    async with async_session() as session:
+        async with session.begin():
+            session.add(user_model)
+            session.commit()
+
