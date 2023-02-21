660cc8c3e46098c32ed6e7403431b2bb299d1eaa
Miroslav Hájek <xhajekm@stuba.sk>
2023-02-21 01:18:41 +0100

    Add constraint names to fix downgrade and change quotation marks

--- migrations/versions/0afdb03f9fea_create_users_table.py
+++ migrations/versions/0afdb03f9fea_create_users_table.py
@@ -9,25 +9,24 @@ from alembic import op
 import sqlalchemy as sa
 
 
-# revision identifiers, used by Alembic.
-revision = '0afdb03f9fea'
+revision = "0afdb03f9fea"
 down_revision = None
 branch_labels = None
 depends_on = None
 
 
 def upgrade() -> None:
-    op.create_table('users',
-        sa.Column('id', sa.BigInteger(), nullable=False),
-        sa.Column('email', sa.String(), nullable=False),
-        sa.Column('password', sa.String(), nullable=False),
-        sa.Column('username', sa.String(), nullable=False),
-        sa.PrimaryKeyConstraint('id'),
-        sa.UniqueConstraint('email')
+    op.create_table("users",
+        sa.Column("id", sa.BigInteger(), nullable=False),
+        sa.Column("email", sa.String(), nullable=False),
+        sa.Column("password", sa.String(), nullable=False),
+        sa.Column("username", sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("email")
     )
-    op.create_index(op.f('ix_users_id'), 'users', ['id'], unique=False)
+    op.create_index(op.f("ix_users_id"), "users", ["id"], unique=False)
 
 
 def downgrade() -> None:
-    op.drop_index(op.f('ix_users_id'), table_name='users')
-    op.drop_table('users')
+    op.drop_index(op.f("ix_users_id"), table_name="users")
+    op.drop_table("users")
--- migrations/versions/c75ee2f000a5_create_patient_table.py
+++ migrations/versions/c75ee2f000a5_create_patient_table.py
@@ -15,80 +15,80 @@ import string
 import random
 
 
-# revision identifiers, used by Alembic.
-revision = 'c75ee2f000a5'
-down_revision = '290bbfb151b1'
+revision = "c75ee2f000a5"
+down_revision = "290bbfb151b1"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
     schema_upgrades()
-    if context.get_x_argument(as_dictionary=True).get('data', None):
+    if context.get_x_argument(as_dictionary=True).get("data", None):
         data_upgrades()
 
 
 def downgrade():
-    if context.get_x_argument(as_dictionary=True).get('data', None):
+    if context.get_x_argument(as_dictionary=True).get("data", None):
         data_downgrades()
     schema_downgrades()
 
 
 def schema_upgrades():
-    op.create_table('patients',
-        sa.Column('id', sa.String(length=20), nullable=False),
-        sa.Column('forename', sa.String(), nullable=False),
-        sa.Column('surname', sa.String(), nullable=False),
-        sa.Column('created_at', sa.DateTime(), nullable=False),
-        sa.Column('created_by', sa.Integer(), nullable=False),
-        sa.Column('modified_at', sa.DateTime()),
-        sa.Column('modified_by', sa.Integer()),
-        sa.ForeignKeyConstraint(['created_by'], ['users.id']),
-        sa.ForeignKeyConstraint(['modified_by'], ['users.id']),
-        sa.PrimaryKeyConstraint('id')
+    op.create_table(
+        "patients",
+        sa.Column("id", sa.String(length=20), nullable=False),
+        sa.Column("forename", sa.String(), nullable=False),
+        sa.Column("surname", sa.String(), nullable=False),
+        sa.Column("created_at", sa.DateTime(), nullable=False),
+        sa.Column("created_by", sa.Integer(), nullable=False),
+        sa.Column("modified_at", sa.DateTime()),
+        sa.Column("modified_by", sa.Integer()),
+        sa.ForeignKeyConstraint(["created_by"], ["users.id"]),
+        sa.ForeignKeyConstraint(["modified_by"], ["users.id"]),
+        sa.PrimaryKeyConstraint("id")
     )
-    op.create_index(op.f('ix_patients_id'), 'patients', ['id'], unique=False)
+    op.create_index(op.f("ix_patients_id"), "patients", ["id"], unique=False)
 
 
 def schema_downgrades():
-    op.drop_index(op.f('ix_patients_id'), table_name='patients')
-    op.drop_table('patients')
+    op.drop_index(op.f("ix_patients_id"), table_name="patients")
+    op.drop_table("patients")
 
 
 def data_upgrades():
     meta = MetaData()
     meta.reflect(
         bind=op.get_bind(),
-        only=('users', 'patients')
+        only=("users", "patients")
     )
 
-    users_tbl = Table('users', meta)
-    patients_tbl = Table('patients', meta)
+    users_tbl = Table("users", meta)
+    patients_tbl = Table("patients", meta)
 
     op.bulk_insert(users_tbl, [{
-        'id': 1,
-        'email': 'tester@gmail.com',
-        'username': 'tester',
-        'password': argon2.hash('Abcdef123')
+        "id": 1,
+        "email": "tester@gmail.com",
+        "username": "tester",
+        "password": argon2.hash("Abcdef123")
     }])
 
     N = 200
     random.seed(1)
     patients = [
         {
-            'id': ''.join(
+            "id": "".join(
                 random.choices(string.ascii_uppercase + string.digits, k=10)
             ),
-            'forename': random.choice([
-                'Jozef', 'Milan', 'Štefan',
-                'Zuzana', 'Monika', 'Anna'
+            "forename": random.choice([
+                "Jozef", "Milan", "Štefan",
+                "Zuzana", "Monika", "Anna"
             ]),
-            'surname': random.choice([
-                'Kováč', 'Molnár', 'Lukáč',
-                'Novák', 'Polák', 'Hudák'
+            "surname": random.choice([
+                "Kováč", "Molnár", "Lukáč",
+                "Novák", "Polák", "Hudák"
             ]),
-            'created_by': 1,
-            'created_at': datetime.now().replace(microsecond=0)
+            "created_by": 1,
+            "created_at": datetime.now().replace(microsecond=0)
         }
         for i in range(N)
     ]
@@ -96,4 +96,4 @@ def data_upgrades():
 
 
 def data_downgrades():
-    op.execute('delete from patients')
+    op.execute("delete from patients")
