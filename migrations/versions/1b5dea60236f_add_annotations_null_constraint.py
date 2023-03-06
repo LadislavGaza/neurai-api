1a5bbf12cea3520be97c5b9408455bc6b769cef5
Miroslav Hájek <xhajekm@stuba.sk>
2023-03-06 16:05:35 +0100

    NAI-164 Fix migration downgrade by naming constraints without name

--- migrations/versions/1b5dea60236f_add_annotations_null_constraint.py
+++ migrations/versions/1b5dea60236f_add_annotations_null_constraint.py
@@ -10,22 +10,22 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '1b5dea60236f'
-down_revision = '748d5a65f67c'
+revision = "1b5dea60236f"
+down_revision = "748d5a65f67c"
 branch_labels = None
 depends_on = None
 
 
 def upgrade() -> None:
     op.alter_column(
-        'annotations', 
-        'filename',
+        "annotations",
+        "filename",
         existing_type=sa.VARCHAR(),
         nullable=True
     )
     op.alter_column(
-        'annotations', 
-        'file_id',
+        "annotations",
+        "file_id",
         existing_type=sa.VARCHAR(),
         nullable=True
     )
@@ -33,14 +33,14 @@ def upgrade() -> None:
 
 def downgrade() -> None:
     op.alter_column(
-        'annotations', 
-        'file_id',
+        "annotations",
+        "file_id",
         existing_type=sa.VARCHAR(),
         nullable=False
     )
     op.alter_column(
-        'annotations', 
-        'filename',
+        "annotations",
+        "filename",
         existing_type=sa.VARCHAR(),
         nullable=False
     )
--- migrations/versions/748d5a65f67c_add_annotations_unique_pair.py
+++ migrations/versions/748d5a65f67c_add_annotations_unique_pair.py
@@ -10,15 +10,23 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '748d5a65f67c'
-down_revision = '6d40004ab0be'
+revision = "748d5a65f67c"
+down_revision = "6d40004ab0be"
 branch_labels = None
 depends_on = None
 
 
 def upgrade() -> None:
-    op.create_unique_constraint(None, 'annotations', ['name', 'mri_file_id'])
+    op.create_unique_constraint(
+        "ix_annotation_name_mri_file_id",
+        "annotations",
+        ["name", "mri_file_id"]
+    )
 
 
 def downgrade() -> None:
-    op.drop_constraint(None, 'annotations', type_='unique')
+    op.drop_constraint(
+        "ix_annotation_name_mri_file_id",
+        "annotations",
+        type_="unique"
+    )
