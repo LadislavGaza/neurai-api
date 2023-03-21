e5778ea821aa13ed443707e61cbb9c51912dac47
Kristína Čopíková <kristina.copikova@gmail.com>
2023-03-21 20:33:02 +0100

    NAI-293 Add screening migration

---
+++ api/migrations/versions/fac6fc29de89_create_screening_table.py
@@ -0,0 +1,42 @@
+"""create_screening_table
+
+Revision ID: fac6fc29de89
+Revises: b1ba681e573d
+Create Date: 2023-03-21 19:14:30.556217
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fac6fc29de89'
+down_revision = 'b1ba681e573d'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.execute('TRUNCATE TABLE mri_files CASCADE')
+
+    op.create_table('screenings',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('name', sa.String(), nullable=False, unique=True),
+        sa.Column('patient_id', sa.String(length=20), nullable=False),
+        sa.Column('created_at', sa.DateTime(), nullable=False),
+        sa.Column('created_by', sa.Integer(), nullable=False),
+        sa.Column('modified_at', sa.DateTime(), nullable=True),
+        sa.Column('modified_by', sa.Integer(), nullable=True),
+        sa.ForeignKeyConstraint(['created_by'], ['users.id'], ondelete='CASCADE'),
+        sa.ForeignKeyConstraint(['modified_by'], ['users.id'], ondelete='CASCADE'),
+        sa.ForeignKeyConstraint(['patient_id'], ['patients.id'], ),
+        sa.PrimaryKeyConstraint('id')
+    )
+    op.add_column('mri_files', sa.Column('screening_id', sa.Integer(), nullable=False))
+    op.create_foreign_key('mri_files_screening_id_fk', 'mri_files', 'screenings', ['screening_id'], ['id'], ondelete='CASCADE')
+
+
+def downgrade() -> None:
+    op.drop_constraint('mri_files_screening_id_fk', 'mri_files', type_='foreignkey')
+    op.drop_column('mri_files', 'screening_id')
+    op.drop_table('screenings')
